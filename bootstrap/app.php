2e64c3cfa6273e296c444654c0371e8ebd16f842
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-11-20 14:34:15 +0700

    add lumen generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -102,12 +102,9 @@ $app->middleware([
 // $app->register(App\Providers\EventServiceProvider::class);
 
 $app->register(App\Providers\JsonApiPaginateServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 $app->register(Fruitcake\Cors\CorsServiceProvider::class);
 $app->register(Maatwebsite\Excel\ExcelServiceProvider::class);
-
-if (env('APP_DEBUG')) {
-    $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
-}
 
 /*
 |--------------------------------------------------------------------------
